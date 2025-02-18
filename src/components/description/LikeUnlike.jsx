748d0185c90af9bdc8080cdf8adf480b61265341
Abir Rakshit <abir.raksh@gmail.com>
2025-02-18 13:21:35 +0600

    corrected some mistakes in components

--- src/components/description/LikeUnlike.jsx
+++ src/components/description/LikeUnlike.jsx
@@ -1,19 +1,21 @@
 import React from "react";
+import likeImage from "../../assets/like.svg";
+import unLikeImage from "../../assets/unlike.svg";
 
 export default function LikeUnlike() {
 	return (
-		<div class="flex gap-10 w-48">
-			<div class="flex gap-1">
-				<div class="shrink-0">
-					<img class="w-5 block" src="./assets/like.svg" alt="Like" />
+		<div className="flex gap-10 w-48">
+			<div className="flex gap-1">
+				<div className="shrink-0">
+					<img className="w-5 block" src={likeImage} alt="Like" />
 				</div>
-				<div class="text-sm leading-[1.7142857] text-slate-600">100K</div>
+				<div className="text-sm leading-[1.7142857] text-slate-600">100K</div>
 			</div>
-			<div class="flex gap-1">
-				<div class="shrink-0">
-					<img class="w-5 block" src="./assets/unlike.svg" alt="Unlike" />
+			<div className="flex gap-1">
+				<div className="shrink-0">
+					<img className="w-5 block" src={unLikeImage} alt="Unlike" />
 				</div>
-				<div class="text-sm leading-[1.7142857] text-slate-600">100K</div>
+				<div className="text-sm leading-[1.7142857] text-slate-600">100K</div>
 			</div>
 		</div>
 	);
