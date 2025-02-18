fb9222d6e7adf6a33d2b3b77f4147362b5983f75
Abir Rakshit <abir.raksh@gmail.com>
2025-02-18 14:02:24 +0600

    minor technical updates on components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import Search from "./Search";
 import RedLogo from "../../assets/Logo.png";
 import searchIcon from "../../assets/search.svg";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
 	return (
 		<nav className="bg-slate-100 shadow-md">
 			<div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3">
-				<a href="/">
+				<Link to="/">
 					<img className="h-10 redLogo" src={RedLogo} alt="RedLogo" />
-				</a>
+				</Link>
 				<div className="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
-					{/* <!-- search --> */}
 					<Search />
 
 					<img
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import Navbar from "../components/navbar/Navbar";
 import Tags from "../components/tags/Tags";
 import VideoGrid from "../components/videoGrid/VideoGrid";
 import Pagination from "../components/ui/Pagination";
-import Footer from "../components/ui/Footer";
 
 export default function Home() {
 	return (
