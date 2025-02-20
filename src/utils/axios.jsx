901c88c34c4923ef66bb250e784e9e841f1542ba
Abir Rakshit <abir.raksh@gmail.com>
2025-02-20 14:31:44 +0600

    updated videos feature and Home page

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "https://localhost:9000",
+	baseURL: "http://localhost:9000",
 	timeout: 1000,
 	headers: { "X-Custom-Header": "foobar" },
 });
