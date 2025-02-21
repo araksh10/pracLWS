d621120c12f4e1f33dbca871c62e455054d57645
Abir Rakshit <abir.raksh@gmail.com>
2025-02-21 15:13:58 +0600

    added related video Slice and updated Video page

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import videosReducer from "../features/videos/VideosSlice";
+import videoReducer from "../features/video/VideoSlice";
 import tagsReducer from '../features/tags/TagsSlice';
+import relatedVideosReducer from '../features/relatedVideos/relatedVideosSlice';
 
 export const store = configureStore({
   reducer: {
     videos: videosReducer,
+    video: videoReducer,
     tags: tagsReducer,
+    relatedVideos: relatedVideosReducer,
   },
 });
--- src/components/description/LikeUnlike.jsx
+++ src/components/description/LikeUnlike.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import likeImage from "../../assets/like.svg";
 import unLikeImage from "../../assets/unlike.svg";
 
-export default function LikeUnlike() {
+export default function LikeUnlike({ likes, unlikes }) {
 	return (
 		<div className="flex gap-10 w-48">
 			<div className="flex gap-1">
 				<div className="shrink-0">
 					<img className="w-5 block" src={likeImage} alt="Like" />
 				</div>
-				<div className="text-sm leading-[1.7142857] text-slate-600">100K</div>
+				<div className="text-sm leading-[1.7142857] text-slate-600">
+					{likes}
+				</div>
 			</div>
 			<div className="flex gap-1">
 				<div className="shrink-0">
 					<img className="w-5 block" src={unLikeImage} alt="Unlike" />
 				</div>
-				<div className="text-sm leading-[1.7142857] text-slate-600">100K</div>
+				<div className="text-sm leading-[1.7142857] text-slate-600">
+					{unlikes}
+				</div>
 			</div>
 		</div>
 	);
